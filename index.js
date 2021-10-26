bb4266d7b1fe1a93a8dff43382b19aa0e900b8a7
osamamagdy <osamamagdy174@gmail.com>
2021-10-26 20:54:32 +0200

    initial files

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const redis = require('redis');
+const app = express();
+
+const client = redis.createClient(
+    {
+        host : "redis-server",
+        port : 6379
+    }
+);
+
+client.set('visits', 0);
+
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
+
+var app_port = app.listen(process.env.PORT || 8000);
+
+
+app.listen(app_port,()=>{
+    console.log("Listening on port 8000");
+});
+
+
+app.get('/',(req,res)=>{
+    //This will be executed with every request to the node server
+    client.get('visits', (err, visits)=>{
+        client.set('visits', parseInt(visits)+1);
+        res.render('home', {  style:"home"  , visits: visits});
+        //Increase the number of visitors by 1
+    });
+});
